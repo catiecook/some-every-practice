9548d89db3616da3befd7956849f6f30796b3271
jaimielogan <jaimielogan@gmail.com>
2016-09-01 17:41:54 -0600

    Some and Every solutions

--- every.js
+++ every.js
@@ -9,27 +9,54 @@ module.exports = {
 // are even numbers.
 
 function allEven (input) {
-  return input;
-};
+  return input.every(function (values){
+    return values % 2 === 0;
+  });
+}
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType (input) {
-  return input;
-};
+  var datatype = typeof input[0];
+  return input.every(function(data){
+    return typeof data === datatype;
+  });
+}
 
 // Check to see if every element in the matrix is
 // an array and that every element in the array is
 // greater than 0.
 
 function positiveMatrix (input) {
-  return input;
-};
+  return input.every(function(data){
+    return data.every(function(element){
+      return element > 0 && Array.isArray(data);
+    });
+  });
+}
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
-function allSameVowels (input) {
-  return input;
+var vowel = function(input){
+	var vowels = [];
+  	for (var i = 0; i < input.length; i++) {
+    	for (var j = 0; j < input[i].length; j++) {
+    		if(/[aeiou]/.test(input[i][j])){
+    			vowels.push(input[i][j]);
+    		}
+    	}
+  	}
+	for (var k = 1; k < vowels.length; k++){
+		if(vowels[k] !== vowels[0])
+		return false;
+	}
+	return true;
 };
+
+function allSameVowels (input) {
+  return input.every(function(data){
+    return typeof data === 'string' && vowel(data);
+  });
+}
--- some.js
+++ some.js
@@ -5,30 +5,41 @@ module.exports = {
   lostCarcosa : lostCarcosa
 };
 
-// Check to see if any of the elements in the 
+// Check to see if any of the elements in the
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
-};
+  return input.some(function(values){
+    return values > 10;
+  });
+}
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
-};
+  return input.some(function(data){
+    return data.length > 10;
+  });
+}
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
-};
+  return input.some(function(data){
+    return data.some(function(element){
+      return element === true;
+    });
+  });
+}
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
-};
+  var substring = 'Lost';
+  return input.some(function(data){
+    return data.indexOf(substring) !== -1; 
+  });
+}
